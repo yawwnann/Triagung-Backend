d066de8193a8c52dc069546fdf08d41bdf59823f
yawwnann <c0478592@gmail.com>
2025-07-21 02:35:33 +0700

    Update CORS configuration to allow all origins for API requests, simplifying access during development.

--- config/cors.php
+++ config/cors.php
@@ -3,13 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://triagung-frontend.vercel.app',
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:5173',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
