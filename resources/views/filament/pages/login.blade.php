5449807192542a3d93f6efecd33bda396581bc0a
yawwnann <c0478592@gmail.com>
2025-07-20 23:26:06 +0700

    Enhance login view by replacing checkbox component with custom HTML for better styling and flexibility

--- resources/views/filament/pages/login.blade.php
+++ resources/views/filament/pages/login.blade.php
@@ -3,7 +3,10 @@
         <form wire:submit.prevent="authenticate">
             <x-filament::input label="Email" wire:model.defer="email" type="email" required autofocus />
             <x-filament::input label="Password" wire:model.defer="password" type="password" required />
-            <x-filament::checkbox label="Remember me" wire:model.defer="remember" />
+            <label class="flex items-center space-x-2">
+                <input type="checkbox" wire:model.defer="remember" />
+                <span>Remember me</span>
+            </label>
             <x-filament::button type="submit" class="w-full">Login</x-filament::button>
         </form>
     </x-filament::card>
