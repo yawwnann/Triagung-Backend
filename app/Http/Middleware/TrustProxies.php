dc36f333ad43e70f14abf1ebdd9c3f6681aaea1c
yawwnann <c0478592@gmail.com>
2025-07-21 01:27:55 +0700

    Implement URL scheme enforcement for development environment in AppServiceProvider to ensure consistent behavior across environments.

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = -1;
+}
