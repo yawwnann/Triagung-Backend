e20193c8a7b878d2ad4670105271b788e35036f5
yawwnann <c0478592@gmail.com>
2025-07-21 02:44:04 +0700

    Add CORS middleware to application configuration to enhance API request handling.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(HandleCors::class);
         // $middleware->web(append: [
         //     \App\Http\Middleware\ForceHttps::class,
         // ]);
